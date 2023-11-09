69a22d48bbc45b6a6b8d16c87a42ca5fb0ca9193
Diego Fleitas <DiegoFleitas@users.noreply.github.com>
2023-11-09 13:30:14 -0300

    Update run_once_after_install.sh

--- run_once_after_install.sh
+++ run_once_after_install.sh
@@ -3,4 +3,9 @@
 # Set default shell to zsh
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
+# Switch to zsh for the current session
+if [ -n "$ZSH_VERSION" ]; then
+    exec zsh
+fi
+
 # Any other final setup steps can be added here
