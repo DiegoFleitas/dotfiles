bf4f50af84edf27916b67707bf2c8d9c42f73000
Diego Fleitas <diego.fleitas68@gmail.com>
2023-11-09 14:22:54 -0300

    Update shell to zsh after install

--- run_once_after_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Set default shell to zsh for new login sessions
-# chsh -s "$(which zsh)"
-exec zsh
---
+++ run_once_z_after_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set default shell to zsh
+sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+
+# Switch to zsh for the current session
+if [ -n "$ZSH_VERSION" ]; then
+    exec zsh
+fi
+
+# Any other final setup steps can be added here
