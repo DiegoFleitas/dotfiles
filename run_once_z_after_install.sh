c0a24007aa1404bd7177cf51f153d5cc2be91999
Diego Fleitas <diego.fleitas68@gmail.com>
2023-12-07 13:49:06 -0300

    Update default shell to zsh in run_once_z_after_install.sh

--- run_once_z_after_install.sh
+++ run_once_z_after_install.sh
@@ -2,12 +2,12 @@
 
 # Attempt to set default shell to zsh if not already set
 if [ "$SHELL" != "/usr/bin/zsh" ]; then
-  chsh -s "/usr/bin/zsh"
+  chsh -s $(which zsh)
 fi
 
 # Exec zsh if in a bash session, which is the default for chezmoi
 if [ -z "$ZSH_VERSION" ]; then
-    exec zsh
+  exec zsh
 fi
 
 # Any other final setup steps can be added here
