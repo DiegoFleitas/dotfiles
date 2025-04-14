8be91e181a24ee61859757e8a3e62327f8aa826f
Diego Fleitas <diego.fleitas68@gmail.com>
2025-04-14 12:10:19 -0300

    add updates

--- run_once_z_after_install.sh
+++ run_once_z_after_install.sh
@@ -16,4 +16,26 @@ if [ -z "$ZSH_VERSION" ]; then
   exec zsh
 fi
 
+# Update nvm to the latest version
+output_message "Updating nvm to the latest version..."
+nvm install --lts && nvm alias default lts
+
+# Update Homebrew and its packages
+output_message "Updating Homebrew and its packages..."
+brew update && brew upgrade
+
+# Update oh-my-zsh
+output_message "Updating oh-my-zsh..."
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    zsh -ic "omz update"
+fi
+
+# Update pyenv and Python versions
+output_message "Updating pyenv and Python versions..."
+brew upgrade pyenv
+if pyenv versions | grep -q "3.10.9"; then
+    output_message "Updating Python 3.10.9..."
+    pyenv install --skip-existing 3.10.9
+fi
+
 # Any other final setup steps...
